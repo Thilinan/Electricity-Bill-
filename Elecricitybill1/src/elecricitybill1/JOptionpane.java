0b051546800f334e3d59767253064c356df18f33
thilina nuwan <bthilina624@gmail.com>
2024-06-20 20:39:27 -0700

    second commit

---
+++ Elecricitybill1/src/elecricitybill1/JOptionpane.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elecricitybill1;
+
+/**
+ *
+ * @author User
+ */
+class JOptionpane {
+
+    static int showConfirmDialog(Object object, String do_you_really_whant_close_this_applicatio, String slect) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showMessageDialog(Object object, String your_name_or_password_incorrect) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
